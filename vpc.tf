f33c2015601a29d523d47ad1924bff3c08dc7d75
gardene10 <gardene.v.c@gmail.com>
2024-09-06 16:08:03 -0300

    added terraform/ in gitignore

--- vpc.tf
+++ vpc.tf
@@ -5,11 +5,27 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "main" {
+/* data "aws_availability_zones" "name" {}
+output "az" {
+value = data.aws_availability_zones.name
+} */
+
+resource "aws_subnet" "subnet-1" {
+  availability_zone = "us-east-1a"
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "subnet-1"
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  availability_zone = "us-east-1b"
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "subnet-1"
+    Name = "subnet-2"
   }
 }
