e40207c67d53632e753b7de4bde2b8518e789b7c
gardene10 <gardene.v.c@gmail.com>
2024-09-07 17:19:30 -0300

    added routeTable and internetGateway

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f6cf40acd3a718b99067bbcdabc0b7fa992dad90573b95534db8bed5baf1aea
-size 560
+oid sha256:8e58a251f47135b0c176cf95b3fb753a6007d3d13f7de7e11789eaf9c79535d0
+size 973
