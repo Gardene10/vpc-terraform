fc6435ebe0ef09ebf402f15a8946740600a8e875
gardene10 <gardene.v.c@gmail.com>
2024-09-13 18:22:23 -0300

    added backend to let save terraform.tfstate in a bucket

--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c96d470fedbe36bfd16f398fafba8a5120310b77452e519c91c1825d391fb83b
-size 203
+oid sha256:de8a9907c9e203e049fdb54b629fa659a1f199154745f02776eb8bccf84e6a91
+size 333
